0602dfa6c58ad0f641771b9c0c3367e06d6a9c0a
WickiThish <dtlwickramathilake@gmail.com>
2026-09-18 13:30:09 +0530

    Header file replaycapture created

---
+++ include/FileReplayCapture.h
@@ -0,0 +1,22 @@
+#ifndef FILEREPLAYCAPTURE_H
+#define FILEREPLAYCAPTURE_H
+
+#include "AttendanceCapture.h"
+#include <string>
+#include <fstream>
+
+class FileReplayCapture:public AttendanceCapture {//filereplay capture inherits from attendancecapture
+private:
+    std::string filePath;
+    std::ifstream fileStream;
+
+public:
+    FileReplayCapture(std::string path);
+
+    void beginSession() override;//ignores parent classes generic function
+    CaptureEvent captureNext() override;
+    void endSession() override;
+    bool hasMoreEvents() const;
+};
+
+#endif
