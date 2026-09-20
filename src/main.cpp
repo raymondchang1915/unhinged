695cb5739bddb42407544b36e242a9cc6059e2c1
raymondchang1915 <63066401+raymondchang1915@users.noreply.github.com>
2026-09-20 13:42:09 +0530

    Add explanatory comments to Administrator and UniversitySystem

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,7 @@ public:
 //checks if it's a student , lect or admin and if any of them are then
 //show the specific menu to them
 //then each spcific menu handle the rest
+//even if a stundet log out the lecture can still stay logged in becuase of the main loop in run()
     void handleException(const SystemException& e){
         cout<<"\n========================================================\n";
         cout<<" SYSTEM ALERT: \n";
